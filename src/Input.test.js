02cd204b44e2c172d36cd3a4a6b67c062af24da6
suchov <artem.suchov@gmail.com>
2019-06-09 17:16:31 +0300

    add some more test prep

--- src/Input.test.js
+++ src/Input.test.js
@@ -4,4 +4,17 @@ import { shallow } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import Input from "./Input";
 
-test("should ", () => {});
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    test("should render component without error", () => {});
+    test("should render input box", () => {});
+    test("should render the submit button", () => {});
+  });
+  describe("word has been guessed", () => {
+    test("should not render component without error", () => {});
+    test("should not render input box", () => {});
+    test("should not render the submit button", () => {});
+  });
+});
+
+describe("update state", () => {});
